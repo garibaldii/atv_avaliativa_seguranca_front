6e26d15ae4e4e029c0d6d1673245a0bc70f74483
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-13 16:47:01 -0300

    Refactor Crypting page: update import path for CesarCryptForm and add title and image

--- src/app/pages/crypting/page.tsx
+++ src/app/pages/crypting/page.tsx
@@ -1,13 +1,17 @@
 "use client"
-import { CesarCryptForm } from "@/components/templates/TabForm"
+import { CesarCryptForm } from "@/components/templates/CesarCryptForm"
+import Image from "next/image"
 
 const Crypting = () => {
 
-   
+
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen w-full relative">
-            <CesarCryptForm/>
+
+            <h1>Criptografia de César</h1>
+            <Image src={"/cesar.png"} width={200} height={200} alt="Julio Cesar"></Image>
+            <CesarCryptForm />
         </div>
 
     )
