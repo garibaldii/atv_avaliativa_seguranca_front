a87768f743af31e39f75cd0598836c82875dcecc
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-14 12:42:57 -0300

    Refactor layout and crypting page: add Cinzel font, update title, and improve structure

--- src/app/pages/crypting/page.tsx
+++ src/app/pages/crypting/page.tsx
@@ -1,19 +1,15 @@
-"use client"
 import { CesarCryptForm } from "@/components/templates/CesarCryptForm"
 import Image from "next/image"
 
+import { cinzel } from "@/app/layout"
+
 const Crypting = () => {
-
-
-
     return (
         <div className="flex flex-col items-center justify-center min-h-screen w-full relative">
-
-            <h1>Criptografia de César</h1>
-            <Image src={"/cesar.png"} width={200} height={200} alt="Julio Cesar"></Image>
+            <Image src={"/cesar.png"} width={200} height={200} alt="Julio Cesar" />
+            <h1 className={`text-2xl font-medium ${cinzel.className}`}>Criptografia de César</h1>
             <CesarCryptForm />
         </div>
-
     )
 }
 
